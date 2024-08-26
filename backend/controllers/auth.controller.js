da3a84c302090225dfb2e6cc7bd2b509dd8620ca
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-08-27 03:49:02 +0530

    adding files

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,42 @@
 import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
-	try {
-		const { fullName, username, password, confirmPassword, gender } = req.body;
+    try {
+        const { fullName, username, password, confirmPassword, gender } = req.body;
         if (password !== confirmPassword) {
-			return res.status(400).json({ error: "Passwords don't match" });
-		}
+            return res.status(400).json({ error: "Passwords don't match" });
+        }
 
         const user = await User.findOne({ username });
+        if (user) {
+            return res.status(400).json({ error: "Username already exists" });
+        }
 
-    }
-    catch(error){
+        //hash password 
 
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+        const newUser = new User({
+            fullName,
+            username,
+            password: hashedPassword,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic,
+        });
+
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ error: "Internal Server Error" });
     }
 };
 
@@ -19,11 +44,11 @@ export const signup = async (req, res) => {
 
 
 
-export const login =(req,res)=>{
+export const login = (req, res) => {
     console.log("loged in up");
 }
 
 
-export const logout =(req,res)=>{
+export const logout = (req, res) => {
     console.log("logout up");
 }
